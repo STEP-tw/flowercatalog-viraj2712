b312ea0bc165c03abb7cb43890f0e91adfa499a8
viraj <virajpatil2712@gmail.com>
2018-01-10 12:10:27 +0530

    Added login feature for registered users. Also added new file with comments and login feature.

--- server.js
+++ server.js
@@ -3,12 +3,32 @@ const http = require('http');
 const WebApp = require('./webapp');
 let toS = o => JSON.stringify(o, null, 2);
 
+let guestBook = fs.readFileSync('public/guestBook.html', 'utf8');
+
+let registered_users = [{
+    userName: 'viraj',
+    name: 'Viraj Patil'
+  },
+  {
+    userName: 'omkar',
+    name: 'Omkar Mote'
+  }
+];
+
 let logRequest = (req, res) => {
   let text = ['------------------------------',
     `${new Date().toLocaleTimeString()}`,
     `${req.method} ${req.url}`,
   ].join('\n');
   console.log(text);
+}
+
+let loadUser = (req, res) => {
+  let sessionid = req.cookies.sessionid;
+  let user = registered_users.find(u => u.sessionid == sessionid);
+  if (sessionid && user) {
+    req.user = user;
+  }
 }
 
 let isGetMethod = function(req) {
@@ -30,59 +50,90 @@ const getContentType = function(fileName) {
     '.js': 'text/javascript',
   };
   return extensions[fileExtension];
-};
+}
 
 let serveFile = function(req, res) {
   if (req.url == '/') req.url = '/index.html';
   let path = './public' + req.url;
   if (isGetMethod(req) && isFile(path)) {
     let contentType = getContentType(path);
-    res.setHeader('Content-type',contentType);
+    res.setHeader('Content-type', contentType);
     res.write(fs.readFileSync(path));
     res.end();
-  } else return;
+  };
 }
 
-const toHtml = function(comment){
+const toHtml = function(comment) {
   let dateAndTime = `<p>Time : ${comment.date} ${comment.time}</p>`
   let userName = `<p>Name : ${comment.name}</p>`;
   let userComment = `<p>Comment : ${comment.comment}</p><br>`;
   return `${dateAndTime} ${userName} ${userComment}`;
 }
 
-const storeCommentInFile = function(comments){
+const storeCommentInFile = function(comments) {
   let allComments = comments.map(toHtml).join('\n');
-  fs.writeFileSync('public/comments.html',allComments);
+  fs.writeFileSync('public/comments.html', allComments);
 }
 
-const getComment = function(req){
+const getComment = function(req) {
   let data = req.body;
-  if(!data.name && !data.comment) return;
+  if (!data.name && !data.comment) return;
   data.time = new Date().toLocaleTimeString();
   data.date = new Date().toDateString();
   return data;
 }
 
-const storeComments = (req,res) => {
+const storeComments = (req) => {
   let data = getComment(req);
-  let comments = fs.readFileSync('data/comments.json','utf8');
+  let comments = fs.readFileSync('data/comments.json', 'utf8');
   comments = JSON.parse(comments);
   comments.unshift(data);
   storeCommentInFile(comments);
-  let commentsToStore = JSON.stringify(comments,null,2);
-  fs.writeFileSync('data/comments.json',commentsToStore);
+  let commentsToStore = JSON.stringify(comments, null, 2);
+  fs.writeFileSync('data/comments.json', commentsToStore);
 }
 
-const redirectToGuestBook = (req,res) => {
-  storeComments(req,res)
+const redirectToGuestBook = (req, res) => {
+  storeComments(req)
   res.redirect('/guestBook.html');
-  res.end();
+}
+
+const redirectLoggedInToAddComment = function(req, res) {
+  if (req.user) {
+    let content = guestBook.replace('currentUser', `User : ${req.user.name}`);
+    res.write(content);
+    res.end();
+  }
+}
+
+const redirectUserToLogin = (req, res) => {
+  if (!req.user) {
+    res.redirect('/loginWithComments.html');
+  } else {
+    res.redirect('/guestBook.html');
+  }
 }
 
 let app = WebApp.create();
 app.use(logRequest);
+app.use(loadUser);
 app.use(serveFile);
-app.post('/commentPage',redirectToGuestBook);
+app.get('/guestBook', redirectUserToLogin);
+app.get('/guestBookPage', redirectLoggedInToAddComment);
+app.post('/commentPage', redirectToGuestBook);
+
+app.post('/login', (req, res) => {
+  let user = registered_users.find(u => u.userName == req.body.name)
+  if (!user) {
+    res.setHeader('Set-Cookie', `logInFailed=true`);
+    res.redirect('/login.html');
+    return;
+  }
+  let sessionid = new Date().getTime();
+  res.setHeader('Set-Cookie', `sessionid=${sessionid}`);
+  user.sessionid = sessionid;
+  res.redirect('/guestBookPage');
+});
 
 const PORT = 5000;
 let server = http.createServer(app);
